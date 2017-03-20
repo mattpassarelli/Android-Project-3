dff7bb0fe684d90e57059b0848df0140b243894f
Matt Passarelli <mattvpassarelli@gmail.com>
2017-03-20 15:39:05 -0400

    Finished the settings activity and XML. I think. There isn't much to do in it

--- app/src/main/java/com/example/matt/project3/MainActivity.java
+++ app/src/main/java/com/example/matt/project3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.matt.project3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,12 +12,18 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final int APP_BAR_ALPHA = 80;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("");
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.getBackground().setAlpha(APP_BAR_ALPHA);
         setSupportActionBar(toolbar);
     }
 
@@ -29,14 +36,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                Intent myIntent = new Intent(this, SettingsActivity.class);
+                startActivity(myIntent);
+                break;
         }
 
         return super.onOptionsItemSelected(item);
