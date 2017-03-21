dc0dc81634247549dc9dc6f73f4a9e82fe794889
Matt Passarelli <mattvpassarelli@gmail.com>
2017-03-21 17:30:50 -0400

    Added network connectivity check

---
+++ app/src/main/java/com/example/matt/project3/CheckNetwork.java
@@ -0,0 +1,32 @@
+package com.example.matt.project3;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class CheckNetwork {
+    Context context;
+
+    CheckNetwork(Context context) {
+        this.context = context;
+    }
+
+    public boolean isNetworkReachable() {
+        ConnectivityManager mManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo current = mManager.getActiveNetworkInfo();
+        if (current == null) {
+            return false;
+        }
+        return (current.getState() == NetworkInfo.State.CONNECTED);
+    }
+
+    public boolean isWifiReachable() {
+        ConnectivityManager mManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo current = mManager.getActiveNetworkInfo();
+        if (current == null) {
+            return false;
+        }
+        return (current.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+}
--- app/src/main/java/com/example/matt/project3/MainActivity.java
+++ app/src/main/java/com/example/matt/project3/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.matt.project3;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner spinner;
     private static final int APP_BAR_ALPHA = 80;
+    private CheckNetwork check;
+    private boolean network, wifi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("");
+
+        check = new CheckNetwork(this);
+        doNetworkCheck();
+        doWirelessCheck();
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -65,9 +75,17 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void doNetworkCheck() {
+        network = check.isNetworkReachable();
+        Toast.makeText(this, "Cellular is " + network, Toast.LENGTH_SHORT).show();
+    }
 
-    //TODO: Method for checking network connectivity
-    //TODO: Create permission requesting method
+    public void doWirelessCheck() {
+        wifi = check.isWifiReachable();
+        Toast.makeText(this, "Wireless is " + wifi, Toast.LENGTH_SHORT).show();
+    }
+
+
     //TODO: Setup threading JSON download
     //TODO: parse JSON
     //TODO: Do stuff with the parsed info (background, etc)
